59516f3587295a2f076d5ae08d7e0ef030d14129
Eyinade Peter <itemize6@gmail.com>
2025-09-15 14:35:33 +0100

    setting database

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -1,5 +1,21 @@
-import { SignIn } from '@clerk/nextjs'
+import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
 
 export default function Page() {
-  return <SignIn />
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 ">
+      <div className="pb-10">
+        <Image
+          src={"/image.png"}
+          alt="Login"
+          width={500}
+          height={500}
+          className="object-contain w-full"
+        />
+      </div>
+      <div className="justify-center flex items-center h-screen">
+        <SignIn />
+      </div>
+    </div>
+  );
 }
