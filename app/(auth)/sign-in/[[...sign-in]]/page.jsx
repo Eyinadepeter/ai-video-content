a57e79c250a6e6dab29320999ba96f9f71f38de4
Eyinade Peter <itemize6@gmail.com>
2025-09-15 14:43:21 +0100

    setting database

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -6,7 +6,7 @@ export default function Page() {
     <div className="grid grid-cols-1 md:grid-cols-2 ">
       <div className="pb-10">
         <Image
-          src={"/image.png"}
+          src={"/WhatsApp Image 2025-09-15 at 14.19.30_2ebafc06.jpg"}
           alt="Login"
           width={500}
           height={500}
