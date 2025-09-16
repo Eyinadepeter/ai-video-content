f9a54893fc9f4621eef6a1d1d6aaca4bba5e0c34
Eyinade Peter <itemize6@gmail.com>
2025-09-16 15:30:04 +0100

    authetivication

--- app/provider.js
+++ app/provider.js
@@ -1,5 +1,5 @@
 import { useUser } from "@clerk/nextjs";
-import React from "react";
+import React, { useEffect } from "react";
 import { db } from "@/configs/db";
 import { Users } from "@/configs/schema";
 import { EmailAddress } from "@clerk/nextjs/server";
@@ -7,8 +7,25 @@ import { EmailAddress } from "@clerk/nextjs/server";
 function provider({ children }) {
   const { user } = useUser();
 
+  useEffect(() => {
+    user && isNewUser();
+  }, [user]);
+
   const isNewUser = async () => {
-    const result = await db.select().from(Users).where(eq(Users.email,user?.primaryEmailAddress?.emailAddress));
+    const result = await db
+      .select()
+      .from(Users)
+      .where(eq(Users.email, user?.primaryEmailAddress?.emailAddress));
+
+    console.log(result);
+
+    if (!result[0]) {
+      await db.insert(Users).values({
+        name: user.fullName,
+        email: user?.primaryEmailAddress?.emailAddress,
+        imageUrl: user?.imageUrl,
+      });
+    }
   };
 
   return <div>{children}</div>;
